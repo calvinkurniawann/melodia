19ff039a2de2f4bbdfe8b1eeac54acfb151abe0e
calvinkurniawann <calvinnkurniawan@gmail.com>
2024-01-15 22:51:48 +0700

    first commit

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,10 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
-        $this->call(BarangSeeder::class);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+        // $this->call(BarangSeeder::class);
     }
 }
